2c2e92430fdbb5a05b0777040a92d0e2b9780005
Prashant Singh <prashant@codeforamerica.org>
2013-10-06 00:54:34 -0700

    Make the server a little more robust

--- server.js
+++ server.js
@@ -7,8 +7,21 @@ var request = require('request');
 
 var server = http.createServer(function (req, res) {
   res.setHeader('Access-Control-Allow-Origin', '*');
-  var url = decodeURIComponent(urlproc.parse(req.url, true).query.url);
-  request.get(url).pipe(res);
+  var encodedUrl = urlproc.parse(req.url, true).query.url;
+  if (!encodedUrl) {
+    res.statusCode = 200;
+    res.end();
+    return;
+  }
+  var url = decodeURIComponent(encodedUrl);
+  try {
+    request.get(url).pipe(res);
+  } catch (e) {
+    console.log(e);
+    res.statusCode = 500;
+    res.end();
+    return;
+  }
 });
 
 var port = process.env.PORT || 3000;
